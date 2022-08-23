b187312e8f051d1a403d52bd172b46b62bdf366c
efiaroca <michel.antonio@eficode.com>
2022-08-24 00:32:06 +0200

    Added post market

--- main.py
+++ main.py
@@ -58,5 +58,3 @@ def index():
    print(f"Post market sum: {pSum}")
 
    return render_template("index.html", data="{:.3f}".format(sumReg), amc=f"Regular market price: {amcPrice} $", ape=f"Regular market price: {apePrice} $", p_data="{:.3f}".format(pSum), p_amc=f"Post market price:{pAmcPrice} $", p_ape=f"Post market price:{pApePrice} $")
-
-app.run()
